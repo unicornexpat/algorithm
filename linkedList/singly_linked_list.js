160f38be94651da576c40d3005c9c608a0180970
Ethan <ethan.nguyens@gmail.com>
2017-02-12 19:04:56 +0000

    add reverse singly linked list usinga array

--- linkedList/singly_linked_list.js
+++ linkedList/singly_linked_list.js
@@ -56,16 +56,84 @@ class LinkedList {
 
 }
 
-let sll = new LinkedList();
+//revert but return new list
+function reverseUsingArray(list) {
+  if (!list.head || !list.head.next) return sll;
 
+  let current = list.head;
+  const values = [];
+
+  while (current) {
+    values.push(current.value);
+    current = current.next;
+  }
+
+  values.reverse();
+
+  let revertedList = new LinkedList();
+
+  values.map(function(value) {
+    revertedList.push(value);
+  });
+
+  return revertedList;
+}
+
+
+let sll = new LinkedList();
 
 //push node
 sll.push(2);
 sll.push(3);
 sll.push(4);
 
-sll.remove(3);
+const revertedList = reverseUsingArray(sll);
+revertedList.head.next.next; //Object {value: 4, next: null}
+
+
+
+//revert - working in progress
+function reverse(list) {
+  if (!list.head || !list.head.next) return sll;
+
+  let current = list.head.next;
+
+  let previous = this.head;
+  let currentCopy;
+
+  while (current.next) {
+    currentCopy = Object.assign({}, current);
+    current.next = previous;
+    previous = currentCopy;
+    console.log(currentCopy);
+    current = currentCopy.next;
+  }
+
+  //old hea now become tail hence need to point to null
+  list.head.next = null;
+
+  //set new head = old tail
+  list.head = current;
+  list.head.next = previous;
+
+  return list;
+}
+
+
+
+let sll2 = new LinkedList();
+
+//push node
+sll2.push(2);
+sll2.push(3);
+sll2.push(4);
+
+sll2.remove(4);
+sll2.push(4);
+
+reverse(sll2);
+
 //check values by traversing LinkedList
-sll.head; //Object {value: 2, next: Object}
-sll.head.next; //Object {value: 3, next: Object}
-sll.head.next.next; //Object {value: 4, next: null}
+sll2.head; //Object {value: 2, next: Object}
+sll2.head.next; //Object {value: 3, next: Object}
+sll2.head.next.next; //Object {value: 4, next: null}
