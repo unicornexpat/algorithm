517a8c6356579cef5c021dde0602934115eda1b1
ethan.nguyen <ethan.nguyens@gmail.com>
2017-02-23 09:33:55 +0000

    correct longest substring

--- letcode/3_longest_substring.js
+++ letcode/3_longest_substring.js
@@ -10,60 +10,26 @@
  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 
-function longest_substring_without_repeating_characters(input) {
-    var chars = input.split('');
-    var curr_char;
-    var str = "";
-    var longest_string = "";
-    var hash = {};
-
-    for (var i = 0; i < chars.length; i++) {
-        curr_char = chars[i];
-        if (!hash[chars[i]]) {
-            str += curr_char;
-            hash[chars[i]] = {index: i};
-        }
-        else {
-            if (longest_string.length <= str.length) {
-                longest_string = str;
-            }
-            var prev_dupeIndex = hash[curr_char].index;
-            var str_FromPrevDupe = input.substring(prev_dupeIndex + 1, i);
-            str = str_FromPrevDupe + curr_char;
-            hash = {};
-            for (var j = prev_dupeIndex + 1; j <= i; j++) {
-                hash[input.charAt(j)] = {index: j};
-            }
-        }
-    }
-    return longest_string.length > str.length ? longest_string : str;
-}
-
 function longestString(s) {
-    let chars = s.split('');
-    let currentChar;
-    let length = 0;
-    let longestLength = 0;
-    let holder = {};
-    let lastIndex = 0;
+    let start = 0;
+    let maxLength = 0;
+    let usedChar = {};
 
-    for (let i = 0; i < chars.length; i++) {
-        currentChar = chars[i];
-        if (holder[currentChar]) {
-            length = i - lastIndex;
-            if (length > longestLength) longestLength = length;
-            lastIndex = i;
+    for (let i in s) {
+        if (usedChar[s[i]] && start <= usedChar[s[i]]) {
+            start = parseInt(usedChar[s[i]]) + 1;
         } else {
-            holder[currentChar] = i;
+            maxLength = Math.max(maxLength, i - start + 1);
         }
+        usedChar[s[i]] = i;
     }
 
-    return longestLength;
+    console.log(usedChar);
 
+    return maxLength;
 }
 
+console.log(longestString("abcabcbb"));
+console.log(longestString("pwwkew"));
 
-console.log(longestString("google.com"));
-
-console.log(longestString("example.com"));
 
