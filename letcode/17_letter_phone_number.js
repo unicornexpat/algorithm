a1134ef3350773c1f7391befb42258e9e05c4641
ethan.nguyen <ethan.nguyens@gmail.com>
2017-02-27 13:04:52 +0000

    edit letter phone number

--- letcode/17_letter_phone_number.js
+++ letcode/17_letter_phone_number.js
@@ -6,3 +6,28 @@
  Input:Digit string "23"
  Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
  */
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+function letterCombinations(digits) {
+    const keys = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+    let res = [];
+
+    for (let i = 0; i < digits.length; i++) {
+        for (let j  = 0; j < digits[i].length; j++){
+            let st = digits[i][j];
+            let k = i + 1;
+            while (k < digits.length) {
+
+            }
+
+        }
+    }
+}
+
+
+
+console.log(letterCombinations([2, 3]));
+
