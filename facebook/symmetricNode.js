c501ee0acd3c3c93e4175dc01a53b0787eeb2c9e
ethan.nguyen <ethan.nguyens@gmail.com>
2018-01-02 12:43:53 +0000

    add symmetricNode solution

---
+++ facebook/symmetricNode.js
@@ -0,0 +1,39 @@
+//Given two identical DOM tree structures, A and B, and a node from A, find the corresponding node in B.
+
+//Return chidlrens of a node in an array
+function getChildrent(node) {
+  return Array.from(node.childNodes);
+}
+
+//Returns an array of indices from a given node
+function getPath(root, node) {
+  const path = [];
+  let currentElement = node;
+
+  while(currentElement !== root && currentElement && currentElement.parentNode) {
+    const index = getChildrent(currentElement.parentNode).indexOf(node);
+    path.push(index);
+    currentElement = currentElement.parentNode;
+  }
+
+  return path;
+
+}
+
+//Return the symmetric node
+function getNodeByPath(root, originalPath) {
+  const path = [...originalPath];
+
+  let element = root;
+
+  while(path.length) {
+    element = getChildrent(element)[path.pop()];
+  }
+
+  return element;
+}
+
+function getSymmetricNode(root1, root2, node) {
+  const path = getPath(root1, node);
+  return getNodeByPath(root2, path);
+}
