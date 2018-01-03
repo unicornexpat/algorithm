fdf72365cae044a4a9443ec950d8094b9e63f02f
ethan.nguyen <ethan.nguyens@gmail.com>
2018-01-03 11:35:39 +0000

    add deep flatten array using array destruction

--- facebook/arrayFlattened.js
+++ facebook/arrayFlattened.js
@@ -14,3 +14,17 @@ const flattened = Array.prototype.concat.apply([], input);
 let flattened = input.reduce(function(a, b) {
   return a.concat(b);
 }, []);
+
+//Deep flatten - Recursively
+const flatten = (deep, flat = []) => {
+  if (deep.length === 0) return flat;
+
+  let [head, ...tail] = deep;
+
+  if (!Array.isArray(head)) return flatten(tail, flat.concat(head));
+  else return flatten(tail, flat.concat(flatten(head)));
+};
+
+const ex = [1, 2, [3, [true, false], 4], 5];
+
+flatten(ex);
