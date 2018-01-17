d93c7c8bcede5ced5d1b7cbb30a8f3fcb799bd19
Ethan <ethan.nguyens@gmail.com>
2018-01-17 22:07:11 +0000

    more solutions for revStr

--- 1.2_reverse_string.js
+++ 1.2_reverse_string.js
@@ -7,3 +7,22 @@ function reverseString(str) {
         return c;
     }).reverse().join('');
 }
+
+//Array reverse
+function revString(str) {
+    return str.split('').reverse().join('');
+}
+
+//Reduce reverse
+function revString(str) {
+    return str.split('').reduce((rev, c) => c + rev);
+}
+
+//Normal Loop O(n);
+function revString(str) {
+    let rev = '';
+    for (let c of str) {
+        rev = c + rev;
+    }
+    return rev;
+}
