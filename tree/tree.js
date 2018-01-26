2a308ee811895653b99e4f7b0a40a921d4ab27ed
ethan.nguyen <ethan.nguyens@gmail.com>
2018-01-26 12:50:49 +0000

    add tree with insertion

---
+++ tree/tree.js
@@ -0,0 +1,53 @@
+//Tree implementation
+
+class Node {
+  constructor(key = null) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(key = null) {
+    this.root = key;
+
+    this.insertNode = this.insertNode.bind(this);
+    this.insert = this.insert.bind(this);
+  }
+
+  //Inserting a node to the tree
+  insert(key) {
+    let newNode = new Node(key);
+    if (this.root === null) this.root = newNode;
+    else this.insertNode(this.root, newNode);
+  };
+
+  insertNode(node, newNode) {
+    if (newNode.key < node.key) {
+      if (node.left === null) node.left = newNode;
+      else this.insertNode(node.left, newNode);
+    } else {
+      if (node.right === null) node.right = newNode;
+      else this.insertNode(node.right, newNode);
+    }
+  };
+}
+
+let tree = new BinarySearchTree();
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(9);
+tree.insert(8);
+tree.insert(10);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
+tree.insert(20);
+tree.insert(18);
+tree.insert(25);
+
+console.log(tree.root.left.key);
