b5dfd5e6fe577c592c8694d1ecfdfb0888dcf73c
Ethan Nguyen <ethan.nguyens@gmail.com>
2021-08-16 19:33:34 +0100

    first bad version

---
+++ letcode/top/sorting_searching/first_bad.js
@@ -0,0 +1,45 @@
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+*/
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+const isBadVersion = function(version) {
+	return version === 4;
+ };
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 0;
+        let right = n;
+        let mid;
+        while (left < right) {
+        	mid = Math.floor(left + right / 2)
+        	if (isBadVersion(mid)) {
+        		right = mid;
+        	} else {
+        		left = mid;
+        	}
+        }
+        return mid;
+    };
+};
+
+console.log(solution(isBadVersion)(6))
